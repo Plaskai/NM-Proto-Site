e5ed9ec7bd904f6a14607b5e8a7f9195a7b73d6f
dylan terrell <dterrell@learnvest.com>
2017-07-07 11:21:33 -0400

    Updated NavBar with incompletes

---
+++ JavaScript/IndexJS.js
@@ -0,0 +1,14 @@
+/*
+!-- Navigation Bar Scroll Function (Incomplete) --!
+$(window).scroll(function() {
+  if ($(".navbar").offset().top > 50) {
+    $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
+});
+.navbar-custom.top-nav-collapse {
+  background: black;
+  padding: 0;
+}
+*/
