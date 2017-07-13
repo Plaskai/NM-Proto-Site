17ca93e24ef85953f4f54487f52445d8662e8179
dylan terrell <dterrell@learnvest.com>
2017-07-13 12:10:04 -0400

    Added pane 5 and 6, edited some css in pane 1

--- JavaScript/IndexJS.js
+++ JavaScript/IndexJS.js
@@ -1,14 +1,11 @@
-/*
-!-- Navigation Bar Scroll Function (Incomplete) --!
 $(window).scroll(function() {
   if ($(".navbar").offset().top > 50) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
+    $(".navbar-default").addClass("top-nav-collapse");
+    $(".menu-btn").addClass("collapse-text");
+    $('.navbar-brand img').attr('src', './images/nmlogo.png');
   } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    $(".navbar-default").removeClass("top-nav-collapse");
+    $(".menu-btn").removeClass("collapse-text");
+    $('.navbar-brand img').attr('src', './images/nmlogoW.png');
   }
 });
-.navbar-custom.top-nav-collapse {
-  background: black;
-  padding: 0;
-}
-*/
